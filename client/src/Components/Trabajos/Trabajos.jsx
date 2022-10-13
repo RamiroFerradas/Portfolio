4fe1e22bb7ae146d89cc1301fdd03fb63f3a85ba
RamiroFerradas <ramiferra97@gmail.com>
2022-10-13 15:23:28 -0300

    update

--- client/src/Components/Trabajos/Trabajos.jsx
+++ client/src/Components/Trabajos/Trabajos.jsx
@@ -3,6 +3,7 @@ import style from "./Trabajos.module.css";
 import pokemon from "../../Assets/trabajos/Captura de pantalla 2022-10-12 190024.png";
 import countries from "../../Assets/trabajos/countries.png";
 import programax from "../../Assets/trabajos/programax.png";
+import videogames from "../../Assets/trabajos/videogames.png";
 import useObserver from "../../Hooks/useObserver";
 
 export default function Trabajos({ trabajos }) {
@@ -82,6 +83,29 @@ export default function Trabajos({ trabajos }) {
                 <a
                   target="_blank"
                   href="https://pi-pokemon-2.vercel.app/"
+                  rel="noreferrer"
+                >
+                  <i className="fa-solid fa-arrow-up-right-from-square"></i>
+                </a>
+              </div>
+            </div>
+            <div className={style.trabajo}>
+              <img
+                src={videogames}
+                alt="videogames"
+                className={style.imgTrabajos}
+              />
+              <div className={style.layer}>
+                <h3>Videogames App</h3>
+                <p>
+                  Esta página es un Proyecto Individual para el bootcamp de
+                  SoyHenry. Se trata de una SPA(single Page Aplication) donde
+                  puede encontrar información sobre diferentes videojuegos, de
+                  multiples plataformas y sus caracteristicas.
+                </p>
+                <a
+                  target="_blank"
+                  href="https://videogamesapp-nine.vercel.app"
                   rel="noreferrer"
                 >
                   <i className="fa-solid fa-arrow-up-right-from-square"></i>
